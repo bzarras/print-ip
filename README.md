812bff884ae1dca17ddb28bb7fc6cd8ac87bd999
bzarras <ben@trysalt.com>
2017-06-24 16:17:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# print-ip
+A command line tool for getting your IP on the LAN
+
+## Install
+```shell
+$ npm install -g print-ip
+```
+
+## Usage
+```shell
+$ ip
+
+172.104.0.3
+```
+or
+```shell
+$ print-ip
+
+172.104.0.3
+```
+
+## Fun
+You can append a color to the end of the command and it will print in that color
+```shell
+$ ip magenta
+
+172.104.0.3 # will be the color magenta
+```
+Valid colors are:
+* `black`
+* `red`
+* `green`
+* `yellow`
+* `blue`
+* `magenta`
+* `cyan`
+* `white`
+* `gray`
---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+
+const os = require('os'),
+  chalk = require('chalk');
+// gets all network interfaces that have been assigned network addresses
+let networkInterfaces = os.networkInterfaces();
+// gets the external IPv4 address
+let addresses = networkInterfaces.en1.filter(address => address.family === 'IPv4' && !address.internal);
+// print the address to the console
+let chalkMethod;
+if (process.argv[2]) {
+  chalkMethod = chalk[process.argv[2]];
+} else {
+  chalkMethod = chalk.green;
+}
+try {
+  addresses.forEach(address => {
+    console.log(chalkMethod(`\n${address.address}\n`));
+  });
+  process.exit(0);
+} catch (err) {
+  // probably an invalid chalk method
+  console.log(chalk.red('Invalid chalk color. Valid colors are:\nblack\nred\ngreen\nyellow\nblue\nmagenta\ncyan\nwhite\ngray'));
+  process.exit(1);
+}
